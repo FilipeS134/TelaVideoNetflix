9fea19987a5915bb33a339712539e335e1b1f1b9
Filipe Silva <filipe134@hotmail.com>
2019-09-18 08:34:51 -0300

    Tela de Video Netflix

---
+++ app/src/main/java/com/example/executandovideo/PlayerActivity.java
@@ -0,0 +1,34 @@
+package com.example.executandovideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        videoView = findViewById(R.id.videoView);
+
+        //esconder a status bar e barra de navegação
+        View decorView = getWindow().getDecorView();
+        int uiOpcoes = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOpcoes);
+
+        //esconder a actionBar
+        getSupportActionBar().hide();
+
+        //executar o video
+        videoView.setMediaController(new MediaController(this));
+         videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video);
+         videoView.start();
+    }
+}
